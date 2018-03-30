20737870aaa77a6fb835f7a4765ddc81cf2c982a
Saiday <feocms@gmail.com>
2018-03-30 17:39:07 +0800

    Add BTCLKK pair

--- main.go
+++ main.go
@@ -10,15 +10,18 @@ import (
 )
 
 func main() {
-	fetchOrderEvery(time.Second, monitorOrderBooks)
+	fetchOrderEvery(time.Second*15, monitorOrderBooks)
 }
 
 func fetchOrderEvery(d time.Duration, f func(string) *entity.OrderPair) {
 	for range time.Tick(d) {
 		lkk := f("LKKUSD")
+		btclkk := f("BTCLKK")
+		btc := f("BTCUSD")
+		arbChances(lkk, btclkk, btc, "BTC")
 		ethlkk := f("ETHLKK")
 		eth := f("ETHUSD")
-		arbChances(lkk, ethlkk, eth)
+		arbChances(lkk, ethlkk, eth, "ETH")
 	}
 }
 
@@ -26,30 +29,30 @@ func fetchOrderEvery(d time.Duration, f func(string) *entity.OrderPair) {
 // pair1 represents target1 order
 // pair2 represetns target2 glove order
 // pair12 represents  target2 / target1 order
-func arbChances(pair1 *entity.OrderPair, pair12 *entity.OrderPair, pair2 *entity.OrderPair) {
-	arbPair1Detail(pair1, pair12, pair2)
-	arbPair1(pair1, pair12, pair2)
-	arbPair2(pair1, pair12, pair2)
+func arbChances(pair1 *entity.OrderPair, pair12 *entity.OrderPair, pair2 *entity.OrderPair, entity string) {
+	// arbPair1Detail(pair1, pair12, pair2)
+	arbPair1(pair1, pair12, pair2, entity)
+	arbPair2(pair1, pair12, pair2, entity)
 }
 
 // e.g. LKK buy: 0.195  sell: 0.207
 //      ETH buy: 908    sell: 909
 //   ETHLKK buy: 4360   sell: 4675
-func arbPair1(pair1 *entity.OrderPair, pair12 *entity.OrderPair, pair2 *entity.OrderPair) {
+func arbPair1(pair1 *entity.OrderPair, pair12 *entity.OrderPair, pair2 *entity.OrderPair, entity string) {
 	sellPair1ToPair2Rate := pair2.Sells[0].Price / pair1.Buys[0].Price
 	if sellPair1ToPair2Rate < pair12.Buys[0].Price {
-		fmt.Printf("!!!!!!!!!!!!!!!!!!!!! chance to arb: sold LKK to usd, buy ETH, sold to LKK: %f >>>> %f\n", sellPair1ToPair2Rate, pair12.Buys[0].Price)
+		fmt.Printf("!!!!!!!!!!!!!!!!!!!!! chance to arb: sold LKK to usd, buy %s, sold to LKK: %f >>>> %f\n", entity, sellPair1ToPair2Rate, pair12.Buys[0].Price)
 	} else {
 		fmt.Printf("no chance to arb LKK: %f > %f\n", sellPair1ToPair2Rate, pair12.Buys[0].Price)
 	}
 }
 
-func arbPair2(pair1 *entity.OrderPair, pair12 *entity.OrderPair, pair2 *entity.OrderPair) {
+func arbPair2(pair1 *entity.OrderPair, pair12 *entity.OrderPair, pair2 *entity.OrderPair, entity string) {
 	sellPair2ToPair1Rate := pair2.Buys[0].Price / pair1.Sells[0].Price
 	if sellPair2ToPair1Rate > pair12.Sells[0].Price {
-		fmt.Printf("!!!!!!!!!!!!!!!!!!!!! chance to arb: sold ETH to usd, buy LKK, sold to ETH : %f >>>> %f\n", sellPair2ToPair1Rate, pair12.Sells[0].Price)
+		fmt.Printf("!!!!!!!!!!!!!!!!!!!!! chance to arb: sold %s to usd, buy LKK, sold to %s : %f >>>> %f\n", entity, entity, sellPair2ToPair1Rate, pair12.Sells[0].Price)
 	} else {
-		fmt.Printf("no chance to arb ETH: %f < %f\n", sellPair2ToPair1Rate, pair12.Sells[0].Price)
+		fmt.Printf("no chance to arb %s: %f < %f\n", entity, sellPair2ToPair1Rate, pair12.Sells[0].Price)
 	}
 }
 
@@ -83,13 +86,7 @@ func arbPair1Detail(pair1 *entity.OrderPair, pair12 *entity.OrderPair, pair2 *en
 	pair12Buys := make([]entity.OrderUnit, len(pair12.Buys))
 	copy(pair12Buys, pair12.Buys)
 
-	fmt.Printf("\n\n\n\n origin")
-	fmt.Printf("%v, len is: %d\n", pair1Sells[0], len(pair1Sells))
-
 	findArbMaximum(&pair1Sells, &pair2Buys, &pair12Buys)
-
-	fmt.Printf("\n\n\n\n final")
-	fmt.Printf("%v, len is: %d\n", pair1Sells[0], len(pair1Sells))
 
 	return 0.0
 }
